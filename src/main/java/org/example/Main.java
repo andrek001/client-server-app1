9b7e66800452c9342b5056efdb97800e5caaccd7
andrek001 <andrewfml@gmail.com>
2023-10-22 23:48:15 +0300

    init commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Server started!");
+        int port = 8090;
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                try (
+                        Socket clientSocket = serverSocket.accept();
+                        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+                        ) {
+                    System.out.println("Новое соединение установлено. Порт клиента: " + clientSocket.getPort());
+                    final String message = in.readLine();
+                    out.println(String.format("Приветствую! От вас получено сообщение: \"%s\". Ваш порт: %d", message, clientSocket.getPort()));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
